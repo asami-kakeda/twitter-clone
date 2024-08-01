cf275dee040a1936f4143c843ab0713392907bc0
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-08-01 19:57:11 +0900

    sidebarの作成

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン */}
+
+      {/* SidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  );
+};
+
+export default Sidebar;
