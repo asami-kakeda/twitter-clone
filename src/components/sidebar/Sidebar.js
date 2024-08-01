835f53ce15fc27756a6988e9e6034019973fa822
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-08-01 20:33:15 +0900

    サイドバーツイートぼたんを作成

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -9,6 +9,7 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
 
 function Sidebar() {
   return (
@@ -32,6 +33,9 @@ function Sidebar() {
       {/* SidebarOption */}
 
       {/* ツイートボタン */}
+      <Button variant="outlined" className="sidebar-tweet">
+        ツイートする
+      </Button>
     </div>
   );
 }
