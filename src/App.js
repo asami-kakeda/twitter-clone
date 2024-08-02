a64119c16161291b00f65ef47a7d1c26d126458f
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-08-02 09:39:19 +0900

    タイムラインコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
+import TimeLine from "./components/timeline/TimeLine";
 
 function App() {
   return (
     <div className="app">
-      <Sidebar></Sidebar>
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Timeline */}
+      <TimeLine />
+
+      {/* Widget */}
     </div>
   );
 }
