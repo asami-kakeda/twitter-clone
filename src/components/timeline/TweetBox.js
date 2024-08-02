cfb9694af2db7671528da40e6d6830319fc8006a
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-08-02 09:53:50 +0900

    ツイートボックスを作成

---
+++ src/components/timeline/TweetBox.js
@@ -0,0 +1,21 @@
+import { Avatar, Button } from "@mui/material";
+import React from "react";
+
+export const TweetBox = () => {
+  return (
+    <div className="tweetBox">
+      <from>
+        <Avatar />
+        <input placeholder="いまどうしてる？" type="text"></input>
+        <input
+          className="tweetBox--imageInput"
+          placeholder="画像のURLを入力してください"
+          type="text"
+        ></input>
+        <Button className="tweetBox-tweetButton" type="submit">
+          ツイートする
+        </Button>
+      </from>
+    </div>
+  );
+};
